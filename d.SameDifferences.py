9ce948ef026ae03bd26fca8c2da9a87c82cf5bfa
Horacio Castro <horaciocastro88@gmail.com>
2026-02-11 18:06:20 -0300

    D.SameDifferences resuelto implementando hashmap

--- d.SameDifferences.py
+++ d.SameDifferences.py
@@ -1,14 +1,29 @@
 
 # D.SameDifferences
 
+# aprobado por Codeforces
+# complejidad O(n) lineal
+
 import sys
+from collections import defaultdict
 
 input = sys.stdin.readline
 
 def resolver(n, vector):
     
-    print(n)
-    print(vector)
+    frecuencias = defaultdict(int)
+
+    # aca va el calculo
+
+    for i in range(n):
+        res_resta = vector[i] - i       # optimizamos evitando un vector de ai - i
+        frecuencias[res_resta] += 1
+    
+    pares = 0
+    for f in frecuencias.values():
+        pares += f * (f - 1) // 2       # usamos // 2 para mantener la vble en int
+        
+    return pares
 
 def main():
 
@@ -20,12 +35,11 @@ def main():
     for i in range(t):
         # carga de datos
         n = int(input())
-        vector = [0] * n
-        for j in range(n):
-            vector[j] = int(input())
+        a = input()
+        vector = list(map(int, a.split()))
             
         print(resolver(n, vector))
-        input()         # frenar la ejecucion si lo necesitamos
+        #input()          frenar la ejecucion si lo necesitamos
 
 if __name__ == "__main__":
     main()
