99b2d0614c6042032b9679b4fc5cc940918c4251
Horacio Castro <horaciocastro88@gmail.com>
2026-03-18 19:32:16 -0300

    Retomamos el problema y vector de posiciones

--- e.ArrangingTheSheep.py
+++ e.ArrangingTheSheep.py
@@ -6,7 +6,27 @@ import sys
 input = sys.stdin.readline
 
 def resolver(n,a):
-    pass
+
+    print("Esta es una prueba del 18 de marzo")
+    print(a)
+    print(len(a))
+
+    if (len(a) != n):
+        print("Cuidado!")
+    else:
+        print("Todo ok")
+
+    vector_posiciones = [] 
+
+    for i in range(len(a)):
+        if a[i] == "*":
+            vector_posiciones.append(i)
+    
+    print(vector_posiciones)    
+
+    input()         # Para frenar la ejecucion
+    
+    return 0
 
 def main():
     t_str = input()
@@ -15,7 +35,7 @@ def main():
     
     for i in range(t):
         n = int(input())
-        a = input().strip()         #eliminamos espacios o saltos de linea al ppio o al final
+        a = input().strip()         #eliminamos espacios o saltos de linea al ppio o al final, por ej el \n
         print(resolver(n, a))
 
 if __name__ == "__main__":
